660ce510e3fe456bff01593721d2428f9b208723
nicole <nikybg96@gmail.com>
2018-06-04 17:53:54 -0400

    editar

--- src/main/java/com/ucbcba/taller/controllers/CommentController.java
+++ src/main/java/com/ucbcba/taller/controllers/CommentController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -58,4 +59,8 @@ public class CommentController {
         commentService.saveComment(comment);
         return "redirect:/showRestaurant/"+comment.getRestaurant().getId();
     }
+
+
+
+
 }
